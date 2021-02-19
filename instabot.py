e1af13ebdd348283a3fadd125b335e6ca61e766d
peterquinn <jl.salaberri@gmail.com>
2021-02-19 22:13:13 +0100

    Viernes 4

--- instabot.py
+++ instabot.py
@@ -3,17 +3,17 @@ from instapy import InstaPy
 import db_functions
 
 # Login
-session = InstaPy(username="j_newroom", password="g0l0ndr1na")
+session = InstaPy(username="picosoyvileta", password="g0l0ndr1na")
 session.login()
 
-my_followers = session.grab_followers(username="j_newroom", amount="full", live_match=True, store_locally=True)
-print(len(my_followers))
-db_functions.send2db(my_followers)
-sleep(600)
+#my_followers = session.grab_followers(username="j_newroom", amount="full", live_match=True, store_locally=True)
+#print(len(my_followers))
+#db_functions.send2db(len(my_followers))
+#sleep(600)
 
 # Settings
 session.set_relationship_bounds(enabled=True, max_followers=980)
 session.set_do_follow(True, percentage=0)
 session.set_do_comment(True, percentage=50)
-session.set_comments(["\U0001F4AA", "nice!!"])
-session.like_by_tags(["trailrunning"], amount=50)
+session.set_comments(["\U0001F4AA", "U+1F60D U+1F970"])
+session.like_by_tags(["puericultura","ripagaina"], amount=50)
